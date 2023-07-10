b3e84ba688d449cf9652acc7cca02f638c35e587
The awer <The awer@DESKTOP-DHSPRFT>
2023-07-10 19:16:27 -0400

    Prueba a la hora de crear clases    -Clase modificada: Enfermedad       Construct, Getters & Setters.    -Se ha eliminado la clase Persona.

--- ProyectoFinal/src/logico/Enfermedad.java
+++ ProyectoFinal/src/logico/Enfermedad.java
@@ -3,4 +3,41 @@ package logico;
 
 public class Enfermedad {
 
+	private String codigo;
+	private String nombre;
+	private ArrayList<String>Sintomas;
+	//private String tipo;
+	private boolean curaEncontrada;
+	
+	public Enfermedad(String codigo, String nombre, ArrayList<String> sintomas, boolean curaEncontrada) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		Sintomas = sintomas;
+		this.curaEncontrada = curaEncontrada;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public ArrayList<String> getSintomas() {
+		return Sintomas;
+	}
+	public void setSintomas(ArrayList<String> sintomas) {
+		Sintomas = sintomas;
+	}
+	public boolean isCuraEncontrada() {
+		return curaEncontrada;
+	}
+	public void setCuraEncontrada(boolean curaEncontrada) {
+		this.curaEncontrada = curaEncontrada;
+	}
 }
